8431c4dbbb7f191c1d45926b28cbe834f116bb3d
Robert Wagner <robert@wagner.id.au>
2024-07-26 15:40:49 +1000

    Removed stray #if

--- src/dbup-redshift/RedshiftScriptExecutor.cs
+++ src/dbup-redshift/RedshiftScriptExecutor.cs
@@ -40,11 +40,7 @@ namespace DbUp.Redshift
             {
                 excuteCommand();
             }
-#if NPGSQLv2
-            catch (NpgsqlException exception)
-#else
             catch (PostgresException exception)
-#endif
             {
                 Log().LogInformation("Npgsql exception has occured in script: '{0}'", script.Name);
                 Log().LogError("Script block number: {0}; Block line {1}; Position: {2}; Message: {3}", index, exception.Line, exception.Position, exception.Message);

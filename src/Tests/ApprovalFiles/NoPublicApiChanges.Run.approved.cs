313d48ba1181aad342f4118611f183f74688e478
Robert Wagner <robert@wagner.id.au>
2025-11-06 13:55:50 +1000

    dbup-core v6.0.15 update (#13)
    * dbup-core v6.0.13
    
    * Test.Common update which included changes to the Surface area test file layout

--- src/Tests/ApprovalFiles/NoPublicApiChanges.Run.approved.cs
+++ src/Tests/ApprovalFiles/NoPublicApiChanges.Run.approved.cs
@@ -1,26 +1,27 @@
-﻿[assembly: System.CLSCompliantAttribute(true)]
-[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
-[assembly: System.Runtime.InteropServices.GuidAttribute("abcc04cc-0bd3-421e-9ae4-9fd0e4f4ef04")]
-
+﻿[assembly: System.CLSCompliant(true)]
+[assembly: System.Reflection.AssemblyMetadata("RepositoryUrl", "https://github.com/DbUp/dbup-redshift.git")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("abcc04cc-0bd3-421e-9ae4-9fd0e4f4ef04")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETCoreApp,Version=v8.0", FrameworkDisplayName=".NET 8.0")]
 public static class RedshiftExtensions
 {
     public static DbUp.Builder.UpgradeEngineBuilder JournalToRedshiftTable(this DbUp.Builder.UpgradeEngineBuilder builder, string schema, string table) { }
     public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(DbUp.Engine.Transactions.IConnectionManager connectionManager) { }
-    public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(this DbUp.Builder.SupportedDatabases supported, string connectionString) { }
     public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(this DbUp.Builder.SupportedDatabases supported, DbUp.Engine.Transactions.IConnectionManager connectionManager) { }
+    public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(this DbUp.Builder.SupportedDatabases supported, string connectionString) { }
     public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(DbUp.Engine.Transactions.IConnectionManager connectionManager, string schema) { }
-    public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(this DbUp.Builder.SupportedDatabases supported, string connectionString, string schema) { }
     public static void RedshiftDatabase(this DbUp.SupportedDatabasesForEnsureDatabase supported, string connectionString) { }
+    public static DbUp.Builder.UpgradeEngineBuilder RedshiftDatabase(this DbUp.Builder.SupportedDatabases supported, string connectionString, string schema) { }
     public static void RedshiftDatabase(this DbUp.SupportedDatabasesForEnsureDatabase supported, string connectionString, DbUp.Engine.Output.IUpgradeLog logger) { }
 }
 namespace DbUp.Redshift
 {
-    public class RedshiftConnectionManager : DbUp.Engine.Transactions.DatabaseConnectionManager, DbUp.Engine.Transactions.IConnectionManager
+    public class RedshiftConnectionManager : DbUp.Engine.Transactions.DatabaseConnectionManager
     {
         public RedshiftConnectionManager(string connectionString) { }
         public override System.Collections.Generic.IEnumerable<string> SplitScriptIntoCommands(string scriptContents) { }
     }
-    public class RedshiftObjectParser : DbUp.Support.SqlObjectParser, DbUp.Engine.ISqlObjectParser
+    public class RedshiftObjectParser : DbUp.Support.SqlObjectParser
     {
         public RedshiftObjectParser() { }
     }
@@ -29,13 +30,13 @@ namespace DbUp.Redshift
         public RedshiftPreprocessor() { }
         public string Process(string contents) { }
     }
-    public class RedshiftScriptExecutor : DbUp.Support.ScriptExecutor, DbUp.Engine.IScriptExecutor
+    public class RedshiftScriptExecutor : DbUp.Support.ScriptExecutor
     {
         public RedshiftScriptExecutor(System.Func<DbUp.Engine.Transactions.IConnectionManager> connectionManagerFactory, System.Func<DbUp.Engine.Output.IUpgradeLog> log, string schema, System.Func<bool> variablesEnabled, System.Collections.Generic.IEnumerable<DbUp.Engine.IScriptPreprocessor> scriptPreprocessors, System.Func<DbUp.Engine.IJournal> journalFactory) { }
         protected override void ExecuteCommandsWithinExceptionHandler(int index, DbUp.Engine.SqlScript script, System.Action excuteCommand) { }
         protected override string GetVerifySchemaSql(string schema) { }
     }
-    public class RedshiftTableJournal : DbUp.Support.TableJournal, DbUp.Engine.IJournal
+    public class RedshiftTableJournal : DbUp.Support.TableJournal
     {
         public RedshiftTableJournal(System.Func<DbUp.Engine.Transactions.IConnectionManager> connectionManager, System.Func<DbUp.Engine.Output.IUpgradeLog> logger, string schema, string tableName) { }
         protected override string CreateSchemaTableSql(string quotedPrimaryKeyName) { }
